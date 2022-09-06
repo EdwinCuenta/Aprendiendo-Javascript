4e34401960a10e16548f700dbd5e32aeaa607a5f
EdwinCuenta <83187876+EdwinCuenta@users.noreply.github.com>
2022-09-05 20:57:31 -0400

    Todo lo necesario para saber en que anio nacio una persona

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-prompt("hola Mundo");
+let nombre;
+let apellido;
+let edad;
+let añoNacio;
+
+
+
+prompt("Digite su nombre");
+prompt("Digite su apellido");
+edad =parseInt (prompt("Digite su edad"));
+
+
+añoNacio = 2022 - edad;
+console.log("Hola "+nombre+" "+apellido+"El año en que nacio fue: "+añoNacio);
+
+
